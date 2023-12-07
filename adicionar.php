c0cbcd3cbab84e1a836337dd8083edbd9ac1f57b
CarlosLincon <carloslincon29@gmail.com>
2023-12-07 17:19:40 -0300

    Dando responsabilidade ao "adicionar.php" para gerar a tabela no sistema

--- adicionar.php
+++ adicionar.php
@@ -2,6 +2,15 @@
 // Conexão com o banco de dados
 $pdo = new PDO('mysql:host=localhost;dbname=tarefas', 'usuario', 'senha');
 
+// Comando SQL para criar a tabela
+$sql = "CREATE TABLE IF NOT EXISTS tarefas (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    tarefa VARCHAR(255) NOT NULL
+)";
+
+// Executa o comando SQL
+$pdo->exec($sql);
+
 // Verifica se o formulário foi submetido
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     // Pega a tarefa do POST
@@ -16,3 +25,4 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 // Redireciona de volta para a página inicial
 header('Location: index.html');
+?>
